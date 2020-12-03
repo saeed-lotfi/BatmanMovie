e1d626f7ec175b9da7a1d05e629328d9c95c82d8
saeed lotfi <saeid0411@gmail.com>
2020-12-03 21:57:27 +0330

    update dependency

--- app/src/main/java/com/bilgiland/batmanmovie/utility/ConstValue.kt
+++ app/src/main/java/com/bilgiland/batmanmovie/utility/ConstValue.kt
@@ -3,6 +3,6 @@ package com.bilgiland.batmanmovie.utility
 object ConstValue {
     const val DATABASE_NAME: String = "movie_database"
     const val BASE_URL = "https://www.omdbapi.com"
-    const val API_KEY = "3e974fca"
+    const val API_KEY = ""
     const val MOVIE_ID = "movie_id"
 }
